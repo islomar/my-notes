7f9749bb54d70779175ee30523105c6c077e5cd2
Isidro Lopez <islomar@gmail.com>
2025-03-27 23:38:42 +0100

    Update oreilly-react-fundamentals: create components as arrow functions and style them

--- oreilly-react-fundamentals-2024/react-vite-project/src/App.tsx
+++ oreilly-react-fundamentals-2024/react-vite-project/src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
 
-function App() {
+const Name = () => {
+    return <p className="name-heading"> Hi, Isi! </p>
+}
 
-  return (
-    <>
-      Hello World
-    </>
-  )
+const ProfilePicture = (props: { imgSrc: string }) => {
+    return <img className="profile-picture" src={props.imgSrc} />;
+}
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <Name/>
+            <ProfilePicture imgSrc="https://cataas.com/cat" />
+        </div>
+    )
 }
 
 export default App
